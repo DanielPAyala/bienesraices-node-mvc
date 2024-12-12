2b696c1203607833ade0d8a45dc185e2511668dd
Carlos Daniel <carlosporrasayala@gmail.com>
2024-12-11 19:09:02 -0500

    Crear User.model.js

---
+++ src/models/User.model.js
@@ -0,0 +1,29 @@
+import { DataTypes } from 'sequelize';
+import db from '../config/db';
+
+const User = db.define('users', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  token: DataTypes.STRING,
+  confirmed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+});
+
+export default User;
