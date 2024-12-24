4634b97da918b22d0ea14aef663e1f96b55d76b1
Carlos Daniel <carlosporrasayala@gmail.com>
2024-12-24 02:24:41 -0500

    Fetch categories and prices for property creation view

--- src/controllers/property.controller.js
+++ src/controllers/property.controller.js
@@ -1,3 +1,6 @@
+import Category from '../models/category.model.js';
+import Price from '../models/price.model.js';
+
 const admin = (req, res) => {
   res.render('property/admin', {
     page: 'Mis Propiedades',
@@ -5,10 +8,17 @@ const admin = (req, res) => {
   });
 };
 
-const create = (req, res) => {
+const create = async (req, res) => {
+  const [categories, prices] = await Promise.all([
+    Category.findAll(),
+    Price.findAll()
+  ]);
+
   res.render('property/create', {
     page: 'Crear Propiedad',
-    navBar: true
+    navBar: true,
+    categories,
+    prices
   });
 };
 
